f31ff478c5a6f7741053759ebdc058d967cb0490
Nepo Rojas <braulio.rojas@islas.org.mx>
2025-01-22 11:28:12 -0800

    🛑🧪😇 Test `extract_players_from_match()`: dummy test

--- tests/testthat/test_calculate_jaccard_index.R
+++ tests/testthat/test_calculate_jaccard_index.R
@@ -7,3 +7,13 @@ describe("Extract players from game", {
     expect_equal(n_obtained_players, n_expected_players)
   })
 })
+
+describe("Extract player from un match", {
+  it("players that played more than ten minutes", {
+    played_minutes <- readr::read_csv("/workdir/tests/data/played_minutes_xolos.csv", show_col_types = FALSE)
+    players_of_xolos <- extract_players_from_match(played_minutes, 1323484)
+    n_obtained_players <- length(players_of_xolos)
+    n_expected_players <- 15
+    expect_equal(n_obtained_players, n_expected_players)
+  })
+})
