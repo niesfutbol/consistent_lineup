cfe4757c152959ffee0cde7299de9ca1ec1c2694
Nepo Rojas <braulio.rojas@islas.org.mx>
2025-01-22 12:32:20 -0800

    ♻️ Extract functions: `filter_players_from_team_with_more_than_10_minutes()` and `.pull_unique_players()`

--- R/calculate_jaccard_index.R
+++ R/calculate_jaccard_index.R
@@ -1,15 +1,23 @@
 extract_players_from_team <- function(played_minutes, team_name) {
+  players_of_team <- .filter_players_from_team_with_more_than_10_minutes(played_minutes, team_name) |>
+    .pull_unique_players()
+}
+
+.filter_players_from_team_with_more_than_10_minutes <- function(played_minutes, team_name) {
   players_of_team <- played_minutes |>
-    dplyr::filter(team == team_name, minutes > 10) |>
+    dplyr::filter(team == team_name, minutes > 10)
+  return(players_of_team)
+}
+
+.pull_unique_players <- function(players_of_team) {
+  unique_players_of_team <- players_of_team |>
     dplyr::pull(player) |>
     unique()
-  return(players_of_team)
+  return(unique_players_of_team)
 }
 
 extract_players_from_match <- function(played_minutes, match_id) {
   players_of_match <- played_minutes |>
     dplyr::filter(match == match_id, minutes > 10) |>
-    dplyr::pull(player) |>
-    unique()
-  return(players_of_match)
+    .pull_unique_players()
 }
