227cf84c4f4c83987488fcc98c114ec9df1581ee
Nepo Rojas <braulio.rojas@islas.org.mx>
2025-01-22 10:12:55 -0800

    ✅ Pass test

--- R/calculate_jaccard_index.R
+++ R/calculate_jaccard_index.R
@@ -1,6 +1,6 @@
 extract_players_from_team <- function(played_minutes, team_name) {
   players_of_team <- played_minutes |>
-    dplyr::filter(team == team_name) |>
+    dplyr::filter(team == team_name, minutes > 10) |>
     dplyr::pull(player) |>
     unique()
   return(players_of_team)
